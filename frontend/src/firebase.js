f36b479c5e3f801a25eb9597878992d267be032a
haithem <haithemfarjallah2002@gmail.com>
2024-04-30 01:42:51 +0100

    fix bug when verifying email and creating account with google account

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
+  apiKey: process.env.FIREBASE_KEY || "AIzaSyDBB-myE93_5WiKDdt9Fka-QQRk8AI3_Rk",
   authDomain: "bookstore-app-47ae6.firebaseapp.com",
   projectId: "bookstore-app-47ae6",
   storageBucket: "bookstore-app-47ae6.appspot.com",
