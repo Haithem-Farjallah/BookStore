d362555cac6b338ab85528278474652507e44e28
Haithem Farjallah <156780419+Haithem-Farjallah@users.noreply.github.com>
2024-04-30 01:56:44 +0100

    Update nodeMailer.js

--- backend/utils/nodeMailer.js
+++ backend/utils/nodeMailer.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.UserMail,
-    pass: process.env.UserPass,
+    pass: "euiinvuasuioxitz",
   },
 });
 
